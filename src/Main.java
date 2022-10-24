19c897b3263d8fb4587da8b50a1e2acc96ce5c12
thiagomars <r15.thiagomaques@alu.ufc.br>
2022-10-24 15:22:56 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Double> notas = new ArrayList<>();
+        notas.add(0.55);
+        //Lista que não pode ser alterada
+        List<Double> notasFixas = Arrays.asList(1.1, 2d, 4.405);
+
+        System.out.println(notas);
+        System.out.println(notasFixas);
+    }
+}
