bbce3cac9e94cc2801a99c2affdf1f8e9aab1228
thiagomars <r15.thiagomaques@alu.ufc.br>
2022-10-29 16:22:16 -0300

    Função para mostrar um item por vez e a função 'remove';

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
         ArrayList<Double> notas = new ArrayList<>();
-        notas.add(0.55);
+        notas.add(0d);
+        notas.add(8.5);
+        notas.add(10d);
+        notas.add(5d);
+        notas.add(6d);
+        notas.add(6.5);
         //Lista que não pode ser alterada
         List<Double> notasFixas = Arrays.asList(1.1, 2d, 4.405);
 
         System.out.println(notas);
         System.out.println(notasFixas);
-        System.out.println(notasFixas);
+        System.out.println("Posição da nota 5,0 eh: " + notas.indexOf(5d));
+
+        //substituir valor
+        notas.set(3, 5.2);
+        System.out.println(notas);
+
+        //mostrar tudo, um por vez
+        for (Double nota : notas) {
+            System.out.println(">> " + nota);
+        }
+
+        System.out.println("maior nota: " + Collections.max(notas));
+        System.out.println("menor nota: " + Collections.min(notas));
+
+        notas.remove(6d);
     }
 }
